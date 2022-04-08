1a2aa1af76af08116d3f799f894e357f4584013b
BeGil <byronegil@gmail.com>
2022-04-08 16:53:02 -0700

    Update key

--- app/src/main/java/com/example/quintrixgroupproject/translation/TranslateAPI.kt
+++ app/src/main/java/com/example/quintrixgroupproject/translation/TranslateAPI.kt
@@ -7,7 +7,6 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 //url https://translate.yandex.net/api/v1.5/tr.json/translate
-//key trnsl.1.1.20220408T232953Z.0568da5ef27a5fd2.c4d2e672f759a28bb7570f3514094d74dcb0b88e
 interface TranslateAPI {
     @GET("/translate?key=${BuildConfig.YANDEX_KEY}&text={text}&lang={source_lang}-{target_lang}")
     fun translateToFrom(
